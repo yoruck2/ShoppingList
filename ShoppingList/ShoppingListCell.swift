6dc2f1a1c3dd19aa7c102a4732f9639b7b3390fb
yoruck2 <yoruck2@gmail.com>
2024-05-26 19:00:28 +0900

    커스텀 셀 설정을 위한 ShoppingListCell 추가

--- ShoppingList/ShoppingListCell.swift
+++ ShoppingList/ShoppingListCell.swift
@@ -8,16 +8,38 @@
 import UIKit
 
 class ShoppingListCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    @IBOutlet var checkButton: UIButton!
+    @IBOutlet var starButton: UIButton!
+    @IBOutlet var shoppingListCellLabel: UILabel!
+    @IBOutlet var shoppingListCellView: UIView!
+    
+    static var identifier: String {
+        return String(describing: self)
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func setUpCell() {
+        self.shoppingListCellView.layer.cornerRadius = 10
+        self.shoppingListCellView.backgroundColor = #colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.968627451, alpha: 1)
+        setUpCheckButton()
+        setUpStarButton()
     }
-
+    
+    func setUpCheckButton() {
+        var buttonConfiguration = UIButton.Configuration.plain()
+        buttonConfiguration.imagePadding = 8
+        checkButton.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
+        checkButton.setImage(UIImage(systemName: "checkmark.square.fill"), for: .selected)
+        checkButton.tintColor = .black
+        checkButton.configuration = buttonConfiguration
+    }
+    
+    func setUpStarButton() {
+        var buttonConfiguration = UIButton.Configuration.plain()
+        buttonConfiguration.imagePadding = 8
+        starButton.setImage(UIImage(systemName: "star"), for: .normal)
+        starButton.setImage(UIImage(systemName: "star.fill"), for: .selected)
+        starButton.tintColor = .black
+        starButton.configuration = buttonConfiguration
+    }
 }

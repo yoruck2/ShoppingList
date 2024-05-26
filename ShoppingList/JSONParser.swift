a4c653fb7d54799bea106ed3259cc6ba6c36ab8a
yoruck2 <yoruck2@gmail.com>
2024-05-26 18:59:05 +0900

    커스텀 객체 저장을 위한 JSONParser 추가

--- ShoppingList/JSONParser.swift
+++ ShoppingList/JSONParser.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct JSONParser {
+    
+    func saveUserDefaults(_ sender: [Product]) {
+        let encoder: JSONEncoder = JSONEncoder()
+    
+        if let shoppingList = try? encoder.encode(sender) {
+            UserDefaults.standard.set(shoppingList, forKey: "shoppingList")
+            print("Save")
+        }
+    }
+    
+    func readUserDefaults() -> [Product] {
+        let decoder: JSONDecoder = JSONDecoder()
+        if let result = UserDefaults.standard.object(forKey: "shoppingList") as? Data,
+           let shoppingList = try? decoder.decode([Product].self, from: result) {
+            return shoppingList
+        }
+        return []
+    }
+}

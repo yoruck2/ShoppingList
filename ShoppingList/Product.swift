3ba41000c1d9688ff0ee5a350302715780701ac2
yoruck2 <yoruck2@gmail.com>
2024-05-26 18:59:44 +0900

    데이터 모델 Product 추가

--- ShoppingList/Product.swift
+++ ShoppingList/Product.swift
@@ -5,9 +5,7 @@
 //  Created by dopamint on 5/26/24.
 //
 
-import Foundation
-
-struct Product {
+struct Product: Codable {
     var name: String = ""
     var isChecked: Bool = false
     var isGetStar: Bool = false
